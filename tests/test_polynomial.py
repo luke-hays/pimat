cd65045dc1ecaca77d460522b51076fd0a731189
Luke Hays <lukehays32@gmail.com>
2024-12-23 12:35:19 -0500

    rename some code in test_polynomial

--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -2,13 +2,13 @@ import pytest
 from math import ceil
 from pymath.polynomial import Polynomial, interpolate
 
-interpolation_data = [
+interpolation_test_data = [
   ([(1, 1)], [1]),
   ([(1, 1), (2, 0)], [1, 0]),
   ([(1, 1), (2, 4), (7, 9)], [1, 4, 9])
 ]
 
-def test_add_two():
+def test_add_two_polynomials():
   f = Polynomial([1, 2, 3])
   g = Polynomial([-8, 17, 0, 5])
   assert f + g == Polynomial([-7, 19, 3, 5])
@@ -25,7 +25,7 @@ def test_non_unique_xs_interpolation():
   with pytest.raises(ValueError):
     interpolate([(1,1), (1, 2)])
 
-@pytest.mark.parametrize('p,expected', interpolation_data)
+@pytest.mark.parametrize('p,expected', interpolation_test_data)
 def test_polynomial_interpolation(p, expected):
   f = interpolate(p)
   assert [ceil(f(xi)) for (xi, yi) in p] == expected
